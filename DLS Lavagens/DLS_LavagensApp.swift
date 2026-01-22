47c44651b8e1f9816120ed6fec1c6db1c95e5b6c
Dinisls <dinisls99@gmail.com>
2026-01-22 11:49:22 +0000

    Initial Commit

---
+++ DLS Lavagens/DLS_LavagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  DLS_LavagensApp.swift
+//  DLS Lavagens
+//
+//  Created by Dinis Santos on 22/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct DLS_LavagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DLS LavagensTests/DLS_LavagensTests.swift
@@ -0,0 +1,17 @@
+//
+//  DLS_LavagensTests.swift
+//  DLS LavagensTests
+//
+//  Created by Dinis Santos on 22/01/2026.
+//
+
+import Testing
+@testable import DLS_Lavagens
+
+struct DLS_LavagensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
